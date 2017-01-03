de09975f24b43278f332f31a3aa2996f77f9d6b9
Melvin SCHULTZ <mschultz@kaliop.com>
2017-01-03 05:34:13 -0500

    add vue-router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    component: require('./components/Hello.vue')
+  }, {
+    path: '*',
+    redirect: '/'
+  }]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App }
 })
